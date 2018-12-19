f25cbf531f87100b2be552bd329b3f140af7d417
Gregor Hyneck <science-of-things@gmx.de>
2018-12-19 09:48:25 +0100

    Move logo images from fileadmin to extension

--- Configuration/TypoScript/Setup/lib.header.ts
+++ Configuration/TypoScript/Setup/lib.header.ts
@@ -1,58 +1,29 @@
+[{$pidOfRV} in tree.rootLineIds]
 lib.header = IMAGE
 lib.header{
-    file = fileadmin/default/templates/images/header/headerTswBg.png
-    file.width = 255
-    params = class="img-responsive img-content"
-    stdWrap.typolink.parameter = {$pidOfTSW}
-    altText = Logo
-    layoutKey = picture
-    layout{
-        picture {
-            element = <picture>###SOURCECOLLECTION###<img srcset="###SRC###" ###PARAMS### ###ALTPARAMS### ###SELFCLOSINGTAGSLASH###></picture>
-            source = <source srcset="###SRC###" media="###MEDIAQUERY###" ###SELFCLOSINGTAGSLASH###>
-        }
-    }
-    sourceCollection {
-        large{
-            width = 380
-            maxW = 1140
-            mediaQuery = (min-width: 1170px)
-        }
-        medium{
-            width = 940
-            maxW = 940
-            mediaQuery = (min-width: 940px)
-        }
-        small{
-            width = 720
-            maxW = 720
-            mediaQuery = (min-width: 720px)
-        }
-    }
-}
-
-[PIDinRootline = {$pidOfTSW}]
-lib.header = IMAGE
-lib.header{
-    file = fileadmin/default/templates/images/header/headerTswBg.png
-    stdWrap.typolink.parameter = {$pidOfTSW}
-}
-[global]
-
-    [PIDinRootline = {$pidOfBikepark}]
-lib.header = IMAGE
-lib.header{
-    file = fileadmin/default/templates/images/header/headerBikeparkBg.png
-    file.width = 255
-    stdWrap.typolink.parameter = {$pidOfBikepark}
-}
-[global]
-
-    [PIDinRootline = {$pidOfRV}]
-lib.header = IMAGE
-lib.header{
-    file = fileadmin/default/templates/images/header/headerRvBg.png
-    file.width = 255
+    file = EXT:rvgomaringen/Resources/Public/Images/Header/headerRvBg.png
+    altText = Logo RV Gomaringen
     stdWrap.typolink.parameter = {$pidOfRV}
-        [global]
+    params = class="img-responsive img-content"
+}
+[END]
 
+[{$pidOfTSW} in tree.rootLineIds]
+lib.header = IMAGE
+lib.header{
+    file = EXT:rvgomaringen/Resources/Public/Images/Header/headerTswBg.png
+    altText = Logo Team Steinlach Wiesaz
+    stdWrap.typolink.parameter = {$pidOfTSW}
+    params = class="img-responsive img-content"
+}
+[END]
+
+[{$pidOfBikepark} in tree.rootLineIds]
+lib.header = IMAGE
+lib.header{
+    file = EXT:rvgomaringen/Resources/Public/Images/Header/headerBikeparkBg.png
+    altText = Logo Bikepark Gomaringen
+    stdWrap.typolink.parameter = {$pidOfBikepark}
+    params = class="img-responsive img-content"
+}
+[END]
